57eea93db85c2d393ad96aaf6d62e4b7b7cbc4b3
srvsngh200892 <srvsngh200892@gmail.com>
2017-06-28 10:52:02 +0530

    updated method name in spec

--- spec/services/tracks/nurburgring_spec.rb
+++ spec/services/tracks/nurburgring_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Tracks::Nurburgring, type: :service do
 
   subject(:track) { Tracks::Nurburgring.new }
 
-  describe '#max_speed_factor' do
+  describe '#track_slowness_factor' do
     it 'respond with car factor between 0 to 35' do
-      expect(track.max_speed_factor).to be_between(0, 35)
+      expect(track.track_slowness_factor).to be_between(0, 35)
     end
 
   end
